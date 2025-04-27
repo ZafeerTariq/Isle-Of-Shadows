532d61808658ece8e24b68f633cf308bb16cc792
ZafeerTariq <zafeertariq4@gmail.com>
2025-04-27 15:43:38 +0500

    add inventory UI

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,19 +1,38 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
+	public GameObject inventoryUI;
+	public GameObject textPrefab;
+	private List<GameObject> texts = new List<GameObject>();
+
 	private Dictionary<ResourceType, int> inventory = new Dictionary<ResourceType, int>();
 
 	void Start() {
 		foreach( ResourceType resource in System.Enum.GetValues( typeof(ResourceType) ) ) {
 			inventory[resource] = 0;
+
+			GameObject text = Instantiate( textPrefab, inventoryUI.transform );
+			text.GetComponent<TextMeshProUGUI>().SetText( resource.ToString() + " : " + inventory[resource].ToString() );
+			texts.Add( text );
+		}
+		UpdateUI();
+	}
+
+	private void UpdateUI() {
+		int i = 0;
+		foreach( ResourceType resource in System.Enum.GetValues( typeof(ResourceType) ) ) {
+			texts[i].GetComponent<TextMeshProUGUI>().SetText( resource.ToString() + " : " + inventory[resource].ToString() );
+			i++;
 		}
 	}
 
 	public void AddResource( ResourceType resource, int amount ) {
 		inventory[resource] += amount;
 		Debug.Log( resource.ToString() + " : " + inventory[resource].ToString() );
+		UpdateUI();
 	}
 
 	public int GetAmount( ResourceType resource ) {
@@ -24,6 +43,7 @@ public class Inventory : MonoBehaviour {
 		if( amount <= inventory[resource] ) {
 			inventory[resource] -= amount;
 			Debug.Log( resource.ToString() + " : " + inventory[resource].ToString() );
+			UpdateUI();
 			return true;
 		}
 		return false;
